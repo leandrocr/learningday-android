1ca0c0bfc5f241ee31cb18e3feb29db16cd70dae
Leandro Repolho <leandro.repolho@theiconic.com.au>
2017-03-24 15:34:55 +1100

    Add Activity Detail

---
+++ app/src/main/java/com/theiconic/android/learningday/DetailsActivity.java
@@ -0,0 +1,24 @@
+package com.theiconic.android.learningday;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by lrepolho on 24/3/17.
+ */
+
+public class DetailsActivity extends AppCompatActivity {
+    private TextView name;
+    private TextView description;
+    
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail;
+
+        name = (TextView) findViewById(R.id.name);
+        description = (TextView) findViewById(R.id.description;
+
+    }
+}
